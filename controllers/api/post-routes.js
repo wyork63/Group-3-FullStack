90681bf3a95cf35c6026576aa4b65710b6a0b9bd
wyork <w.york63@gmail.com>
2022-03-02 20:55:17 -0500

    get users

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,39 @@
+const router = require('express').Router();
+const sequelize = require('../../config/connection');
+const { Post, User, Comment, Country } = require('../../models') 
+
+// get all users (from just tech news)
+router.get('/', (req, res) => {
+    console.log('======================');
+    Post.findAll({
+      attributes: [
+        'id',
+        'post_url',
+        'title',
+        'created_at',
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      ],
+      order: [['created_at', 'DESC']],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => res.json(dbPostData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+module.exports = router;
